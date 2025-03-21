05ce26b031ad062bc838f9b603fa34f5f0bcd0b1
Shriyansh Gupta <gshriyansh703@gmail.com>
2025-03-21 23:08:51 +0530

    hands on jwt security filter and jwt authentication

--- Spring-Security-HandsOn/src/main/java/com/capgemini/Spring_Security_HandsOn/controller/UserController.java
+++ Spring-Security-HandsOn/src/main/java/com/capgemini/Spring_Security_HandsOn/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.capgemini.Spring_Security_HandsOn.controller;
 
+import com.capgemini.Spring_Security_HandsOn.model.User;
+import com.capgemini.Spring_Security_HandsOn.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user){
+        // encrypt password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "user register successfully!";
+    }
+
+    @GetMapping("/all")
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
 }
--- Spring-Security-HandsOn/src/main/java/com/capgemini/Spring_Security_HandsOn/model/User.java
+++ Spring-Security-HandsOn/src/main/java/com/capgemini/Spring_Security_HandsOn/model/User.java
@@ -1,4 +1,39 @@
 package com.capgemini.Spring_Security_HandsOn.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String userName;
+
+    private String password;
+
+    public User(){}
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
